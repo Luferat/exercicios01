6eced3bf86a7cbe8b28b453bfab6155100c87137
André Luferat <33375030+Luferat@users.noreply.github.com>
2025-10-07 08:16:38 -0300

    Update Exercicio12.java

--- src/main/java/net/luferat/Exercicio12.java
+++ src/main/java/net/luferat/Exercicio12.java
@@ -7,7 +7,27 @@
 
 package net.luferat;
 
+// Biblioteca que recebe dados do terminal
+import java.util.Scanner;
+
 public class Exercicio12 {
 
+  public static void main(String[] args) {
 
+    // Mensagem e entrada do usuário
+    System.out.print("Digite um número inteiro: ");
+    
+    // Entradas do usuário são SEMPRE Strings
+    Scanner userNum = new Scanner(System.in);
+
+    // Casting: String → int
+    int num = (int) userNum;
+
+    // Lógica
+    if (num % 2 == 0) {
+      System.out.println(num + " é par");
+    } else }
+      System.out.println(num + " é ímpar");
+    }
+  }
 }
